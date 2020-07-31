5a06dff91bb2249edec6d7bba59e0c887e238c3a
shirantha97 <39570216+shirantha97@users.noreply.github.com>
2020-08-01 02:14:01 +0530

    create user model

--- src/app/register/register.page.ts
+++ src/app/register/register.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {User} from '../models/user.model'
+import { async } from '@angular/core/testing';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +10,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterPage implements OnInit {
 
-  constructor() { }
+  user = {} as User
+
+  constructor(private toastCtrl: ToastController) { }
 
   ngOnInit() {
+  }
+
+
+  async register(u){
+    if(this.formValidation()){
+
+    }
+  }
+
+  formValidation(){
+    if(!this.user.email){
+      this.showToast('Enter E-mail')
+      return false;
+    }
+  }
+
+  showToast(msg: string){
+    this.toastCtrl.create({
+      message: msg,
+      duration: 3000,
+      position: "bottom",
+      color: "primary"
+    }).then(toastData =>{
+      toastData.present();
+    })
   }
 
 }
