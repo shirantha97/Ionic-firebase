f6bda1e0b5f36cb25e569215583d5452b03b2158
shirantha97 <39570216+shirantha97@users.noreply.github.com>
2020-08-01 16:17:13 +0530

    Add login and routes

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,4 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../models/user.model';
+import {
+  ToastController,
+  LoadingController,
+  NavController,
+} from "@ionic/angular";
+import { AngularFireAuth } from "@angular/fire/auth";
+
 
 @Component({
   selector: 'app-login',
@@ -7,9 +15,66 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  user = {} as User
+
+  constructor(
+    private toastCtrl: ToastController,
+    private loadCtrl: LoadingController,
+    private afAuth: AngularFireAuth,
+    private navCtrl: NavController
+  ) { }
 
   ngOnInit() {
+  }
+
+  async login(u: User) {
+    console.log(u)
+    if (this.formValidation()) {
+      let loader = this.loadCtrl.create({
+        message: "Please Wait",
+      });
+      (await loader).present();
+
+      try {
+        await this.afAuth
+          .signInWithEmailAndPassword(u.email, u.password)
+          .then((data) => {
+            console.log(data);
+          });
+
+        this.navCtrl.navigateRoot("home");
+      } catch (e) {
+        console.log(e);
+      }
+
+      (await loader).dismiss()
+
+    }
+  }
+
+  formValidation() {
+    if (!this.user.email) {
+      this.showToast("Enter E-mail");
+      return false;
+    }
+    if (!this.user.password) {
+      this.showToast("Enter Password");
+      return false;
+    }
+    return true;
+  }
+
+  showToast(msg: string) {
+    this.toastCtrl
+      .create({
+        message: msg,
+        duration: 3000,
+        position: "bottom",
+        color: "primary",
+      })
+      .then((toastData) => {
+        toastData.present();
+      });
   }
 
 }
--- src/app/register/register.page.ts
+++ src/app/register/register.page.ts
@@ -1,45 +1,77 @@
-import { Component, OnInit } from '@angular/core';
-import {User} from '../models/user.model'
-import { async } from '@angular/core/testing';
-import { ToastController } from '@ionic/angular';
+import { Component, OnInit } from "@angular/core";
+import { User } from "../models/user.model";
+import { async } from "@angular/core/testing";
+import {
+  ToastController,
+  LoadingController,
+  NavController,
+} from "@ionic/angular";
+import { AngularFireAuth } from "@angular/fire/auth";
 
 @Component({
-  selector: 'app-register',
-  templateUrl: './register.page.html',
-  styleUrls: ['./register.page.scss'],
+  selector: "app-register",
+  templateUrl: "./register.page.html",
+  styleUrls: ["./register.page.scss"],
 })
 export class RegisterPage implements OnInit {
+  user = {} as User;
 
-  user = {} as User
+  constructor(
+    private toastCtrl: ToastController,
+    private loadCtrl: LoadingController,
+    private afAuth: AngularFireAuth,
+    private navCtrl: NavController
+  ) {}
 
-  constructor(private toastCtrl: ToastController) { }
+  ngOnInit() {}
 
-  ngOnInit() {
-  }
+  async register(u: User) {
+    console.log(u)
+    if (this.formValidation()) {
+      let loader = this.loadCtrl.create({
+        message: "Please Wait",
+      });
+      (await loader).present();
 
+      try {
+        await this.afAuth
+          .createUserWithEmailAndPassword(u.email, u.password)
+          .then((data) => {
+            console.log(data);
+          });
 
-  async register(u){
-    if(this.formValidation()){
+        this.navCtrl.navigateRoot("home");
+      } catch (e) {
+        console.log(e);
+      }
+
+      (await loader).dismiss()
 
     }
   }
 
-  formValidation(){
-    if(!this.user.email){
-      this.showToast('Enter E-mail')
+  formValidation() {
+    if (!this.user.email) {
+      this.showToast("Enter E-mail");
       return false;
     }
+    if (!this.user.password) {
+      this.showToast("Enter Password");
+      return false;
+    }
+    return true;
   }
 
-  showToast(msg: string){
-    this.toastCtrl.create({
-      message: msg,
-      duration: 3000,
-      position: "bottom",
-      color: "primary"
-    }).then(toastData =>{
-      toastData.present();
-    })
+  showToast(msg: string) {
+    this.toastCtrl
+      .create({
+        message: msg,
+        duration: 3000,
+        position: "bottom",
+        color: "primary",
+      })
+      .then((toastData) => {
+        toastData.present();
+      });
   }
-
 }
