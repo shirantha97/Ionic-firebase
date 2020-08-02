7b996cfec4ef39099df871a9ab99ad0d2b157d74
shirantha97 <39570216+shirantha97@users.noreply.github.com>
2020-08-02 22:22:38 +0530

    add posts to the firestore collection

--- src/app/add-posts/add-posts.page.ts
+++ src/app/add-posts/add-posts.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Post } from '../models/post.model';
+import {ToastController, LoadingController} from '@ionic/angular'
+import { AngularFirestore } from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-add-posts',
@@ -7,9 +10,60 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddPostsPage implements OnInit {
 
-  constructor() { }
+  post = {} as Post;
+  
+  constructor(
+    private toastCtrl: ToastController,
+    private loadCtrl: LoadingController,
+    private firestore: AngularFirestore
+  ) { }
 
   ngOnInit() {
   }
+
+  async createPost(p: Post){
+
+    if(this.formValidation()){
+      let loader = this.loadCtrl.create({
+        message: "Please Wait",
+      });
+  
+      (await loader).present();
+
+      try{
+        await this.firestore.collection('posts').add(p)
+      }catch(e){
+        this.showToast(e)
+      }
+      (await loader).dismiss();
+
+    }    
+  }  
+
+  formValidation() {
+    if (!this.post.title) {
+      this.showToast("Enter a title");
+      return false;
+    }
+    if (!this.post.details) {
+      this.showToast("Enter details");
+      return false;
+    }
+    return true;
+  }
+
+  showToast(msg: string) {
+    this.toastCtrl
+      .create({
+        message: msg,
+        duration: 3000,
+        position: "bottom",
+        color: "primary",
+      })
+      .then((toastData) => {
+        toastData.present();
+      });
+  }
+
 
 }
---
+++ src/app/models/post.model.ts
@@ -0,0 +1,7 @@
+import { StringifyOptions } from 'querystring';
+
+export interface Post{
+    title: string,
+    details: string,
+    uid: string
+}
